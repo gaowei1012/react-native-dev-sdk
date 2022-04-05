2290dece7a77f01d55cb7768bff4cfd80c78849b
gaowei1012 <gyb2020018@163.com>
2022-04-05 20:44:43 +0800

    update: modal

--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -2,7 +2,7 @@
  * @Author: mingwei
  * @Date: 2022-04-05 08:53:27
  * @LastEditors: mingwei
- * @LastEditTime: 2022-04-05 20:40:00
+ * @LastEditTime: 2022-04-05 20:44:36
  * @FilePath: /react-native-dev-sdk/components/Modal/index.tsx
  * @Description:
  */
@@ -10,7 +10,7 @@
 import React from 'react';
 import { Modal } from 'react-native';
 import { ModalProps, ViewStyle, StyleProp } from 'react-native';
-import ModalViews from './ModelView';
+import ModalViews from './ModalView';
 
 interface DevSdkModalProps extends ModalProps {
   modelStyle?: StyleProp<ViewStyle>;
