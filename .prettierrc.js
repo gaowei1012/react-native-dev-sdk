b9e10e819b7a7816a72b1c7b393030c17566f2d0
gaowei1012 <gyb2020018@163.com>
2022-04-05 08:46:35 +0800

    :heavy_plus_sign: 添加项目基础配置

---
+++ .prettierrc.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: mingwei
+ * @Date: 2022-04-05 08:39:22
+ * @LastEditors: mingwei
+ * @LastEditTime: 2022-04-05 08:39:37
+ * @FilePath: /react-native-dev-sdk/.prettierrc.js
+ * @Description:
+ */
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  // semi: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  // jsxSingleQuote: true,
+  singleQuote: true,
+  // trailingComma: 'all',
+  arrowParens: "avoid",
+};
--- README.md
+++ README.md
@@ -1,3 +1,14 @@
+<!--
+ * @Author: mingwei
+ * @Date: 2022-04-04 23:31:26
+ * @LastEditors: mingwei
+ * @LastEditTime: 2022-04-05 08:33:45
+ * @FilePath: /react-native-dev-sdk/README.md
+ * @Description:
+-->
+
 # react-native-dev-sdk
 
 react-native-dev-sdk
+
+> ReactNative SDK, 封装大部业务中常用组件
