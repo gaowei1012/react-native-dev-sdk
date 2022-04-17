2521740b43f994e026b95bf0682f7dcb96d4690a
gaowei1012 <gyb2020018@163.com>
2022-04-17 21:53:11 +0800

    fix: 修改使用样式问题

--- dist/tools/back/style/index.d.ts
+++ dist/tools/back/style/index.d.ts
@@ -1,6 +1,6 @@
 export declare const backStyle: {
     back: {
-        width: any;
-        height: any;
+        width: number;
+        height: number;
     };
 };
--- dist/tools/back/style/index.js
+++ dist/tools/back/style/index.js
@@ -5,16 +5,16 @@ exports.backStyle = void 0;
  * @Author: mingwei
  * @Date: 2022-04-16 16:44:55
  * @LastEditors: mingwei
- * @LastEditTime: 2022-04-17 17:51:31
+ * @LastEditTime: 2022-04-17 21:50:49
  * @FilePath: /react-native-dev-sdk/src/tools/back/style/index.ts
  * @Description:
  */
 var react_native_1 = require("react-native");
 var __1 = require("../..");
-var setPx2dp = __1.Px2dpTools.setPx2dp;
+var setPixel = __1.Px2dpTools.setPixel;
 exports.backStyle = react_native_1.StyleSheet.create({
     back: {
-        width: setPx2dp(18),
-        height: setPx2dp(18),
+        width: setPixel(18),
+        height: setPixel(18),
     },
 });
