649fc9a4b3982cd73e8dd585926f21114fca1134
gaowei1012 <gyb2020018@163.com>
2022-04-17 20:29:38 +0800

    update: dist

--- dist/tools/back/style/index.js
+++ dist/tools/back/style/index.js
@@ -5,15 +5,16 @@ exports.backStyle = void 0;
  * @Author: mingwei
  * @Date: 2022-04-16 16:44:55
  * @LastEditors: mingwei
- * @LastEditTime: 2022-04-16 16:47:08
- * @FilePath: /react-native-dev-sdk/tools/back/style/style.ts
+ * @LastEditTime: 2022-04-17 17:51:31
+ * @FilePath: /react-native-dev-sdk/src/tools/back/style/index.ts
  * @Description:
  */
 var react_native_1 = require("react-native");
 var __1 = require("../..");
+var setPx2dp = __1.Px2dpTools.setPx2dp;
 exports.backStyle = react_native_1.StyleSheet.create({
     back: {
-        width: (0, __1.px2dp)(18),
-        height: (0, __1.px2dp)(18),
+        width: setPx2dp(18),
+        height: setPx2dp(18),
     },
 });
