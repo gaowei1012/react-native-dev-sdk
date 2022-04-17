182957696d01df4391228d3979a34336510c037b
gaowei1012 <gyb2020018@163.com>
2022-04-17 17:46:31 +0800

    add: 添加px2dp

--- src/doc/readme.md
+++ src/doc/readme.md
@@ -2,10 +2,19 @@
  * @Author: mingwei
  * @Date: 2022-04-16 16:32:56
  * @LastEditors: mingwei
- * @LastEditTime: 2022-04-16 16:32:57
- * @FilePath: /react-native-dev-sdk/doc/readme.md
+ * @LastEditTime: 2022-04-17 17:34:27
+ * @FilePath: /react-native-dev-sdk/src/doc/readme.md
  * @Description:
 -->
 
-- tools
--
+> ReactNative Dev SDK
+
+### 安装依赖
+
+> 使用，安装需要将 dev 嵌入项目
+
+```
+  npm i @react-native-async-storage/async-storage @react-navigation/native axios react-native-root-toast --save
+```
+
+> 在项目 ios 目录下，执行 `pod isntall`
--- src/tools/px2dp/px2dp.ts
+++ src/tools/px2dp/px2dp.ts
@@ -2,23 +2,40 @@
  * @Author: mingwei
  * @Date: 2022-04-16 14:44:16
  * @LastEditors: mingwei
- * @LastEditTime: 2022-04-16 16:36:52
- * @FilePath: /react-native-dev-sdk/tools/px2dp/px2dp.ts
+ * @LastEditTime: 2022-04-17 17:45:55
+ * @FilePath: /react-native-dev-sdk/src/tools/px2dp/px2dp.ts
  * @Description:
  */
 
 import { Dimensions } from 'react-native';
 
-// 这里定义设计稿尺寸，这里默认使用一倍图
-const defaultWidth: number = 375;
+class Px2dpTools {
+  /**
+   * 获取设备可见区域宽度
+   * @returns number
+   */
+  static getWidth() {
+    return Dimensions.get('window').width;
+  }
+  /**
+   * 获取设备可见区域高度
+   * @returns number
+   */
+  static getHeight() {
+    return Dimensions.get('window').height;
+  }
+  /**
+   * 设置像素，根据ui尺寸适配。默认一倍图 375
+   * @returns number
+   */
+  static setPx2dp(uiEleWidth: number, defaultWidth?: number) {
+    const width = Dimensions.get('window').width;
+    if (!defaultWidth) {
+      return (uiEleWidth * width) / 375;
+    } else {
+      return (uiEleWidth * width) / defaultWidth;
+    }
+  }
+}
 
-const widtth: number = Dimensions.get('window').width;
-
-const height: number = Dimensions.get('window').height;
-
-// 计算屏幕像素大小
-const px2dp = (uiEleWidth: number) => {
-  return (uiEleWidth * widtth) / defaultWidth;
-};
-
-export { widtth, height, px2dp };
+export { Px2dpTools };
