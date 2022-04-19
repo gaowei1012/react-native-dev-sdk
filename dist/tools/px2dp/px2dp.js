6b8b8fd33631a979835af02d63ec6c3db30f6079
gaowei1012 <gyb2020018@163.com>
2022-04-19 09:11:49 +0800

    update: 优化px2dp

--- dist/tools/px2dp/px2dp.js
+++ dist/tools/px2dp/px2dp.js
@@ -3,7 +3,7 @@
  * @Author: mingwei
  * @Date: 2022-04-16 14:44:16
  * @LastEditors: mingwei
- * @LastEditTime: 2022-04-19 07:54:48
+ * @LastEditTime: 2022-04-19 09:09:53
  * @FilePath: /react-native-dev-sdk/src/tools/px2dp/px2dp.ts
  * @Description:
  */
