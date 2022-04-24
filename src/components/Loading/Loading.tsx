745b1009b86c9704af483308bf0f37962c0d059e
gaowei1012 <gyb2020018@163.com>
2022-04-24 23:14:03 +0800

    add: loading

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -2,7 +2,7 @@
  * @Author: mingwei
  * @Date: 2022-04-24 22:36:22
  * @LastEditors: mingwei
- * @LastEditTime: 2022-04-24 23:01:07
+ * @LastEditTime: 2022-04-24 23:05:52
  * @FilePath: /react-native-dev-sdk/src/components/Loading/Loading.tsx
  * @Description:
  */
