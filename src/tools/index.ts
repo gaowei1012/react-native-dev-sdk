b3292a31c33d568d07341569bc67b874bce1a731
gaowei1012 <gyb2020018@163.com>
2022-04-24 22:10:58 +0800

    删除多余代码

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -2,12 +2,12 @@
  * @Author: mingwei
  * @Date: 2022-04-16 14:36:12
  * @LastEditors: mingwei
- * @LastEditTime: 2022-04-21 18:11:40
+ * @LastEditTime: 2022-04-24 22:09:25
  * @FilePath: /react-native-dev-sdk/src/tools/index.ts
  * @Description:
  */
 
-export * from './navigate/navigation';
+export * from './navigate';
 export * from './px2dp';
 export * from './toast';
 export * from './storage';
