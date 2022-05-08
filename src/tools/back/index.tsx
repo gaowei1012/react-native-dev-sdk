540e91187d814ebe4c56bfcc2dbadfd32667569a
gaowei1012 <gyb2020018@163.com>
2022-05-08 12:39:14 +0800

    修改返回引入image方式

--- src/tools/back/index.tsx
+++ src/tools/back/index.tsx
@@ -2,7 +2,7 @@
  * @Author: mingwei
  * @Date: 2022-04-16 16:37:08
  * @LastEditors: mingwei
- * @LastEditTime: 2022-05-04 22:38:09
+ * @LastEditTime: 2022-05-08 12:37:35
  * @FilePath: /react-native-dev-sdk/src/tools/back/index.tsx
  * @Description:
  */
@@ -12,12 +12,14 @@ import { NavigatorUtils } from '../navigate';
 import { backStyle } from './style';
 import { GoBackProps } from '../../types';
 
+const BackIcon = require('./assets/back.png');
+
 const GoBack: React.FC<GoBackProps> = props => {
   let { action } = props;
   action = action ? action : () => NavigatorUtils.back();
   return (
     <TouchableOpacity activeOpacity={1} onPress={action}>
-      <Image style={backStyle.back} source={require('./assets/back.png')} />
+      <Image style={backStyle.back} source={BackIcon} />
     </TouchableOpacity>
   );
 };
