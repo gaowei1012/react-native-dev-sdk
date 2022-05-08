f08a6de79285b9d37d579ac19750987ff3f04787
gaowei1012 <gyb2020018@163.com>
2022-05-08 12:57:29 +0800

    build

--- dist/tools/back/index.js
+++ dist/tools/back/index.js
@@ -15,10 +15,10 @@ var jsx_runtime_1 = require("react/jsx-runtime");
 var react_native_1 = require("react-native");
 var navigate_1 = require("../navigate");
 var style_1 = require("./style");
-var BackIcon = require('./assets/back.png');
+// const BackIcon = require('./assets/back.png');
 var GoBack = function (props) {
-    var action = props.action;
+    var action = props.action, url = props.url;
     action = action ? action : function () { return navigate_1.NavigatorUtils.back(); };
-    return ((0, jsx_runtime_1.jsx)(react_native_1.TouchableOpacity, __assign({ activeOpacity: 1, onPress: action }, { children: (0, jsx_runtime_1.jsx)(react_native_1.Image, { style: style_1.backStyle.back, source: BackIcon }, void 0) }), void 0));
+    return ((0, jsx_runtime_1.jsx)(react_native_1.TouchableOpacity, __assign({ activeOpacity: 1, onPress: action }, { children: (0, jsx_runtime_1.jsx)(react_native_1.Image, { style: style_1.backStyle.back, source: url }, void 0) }), void 0));
 };
 exports.default = GoBack;
